c7fb321ce0daf6ef7bc0412e746b27611452104a
Maxime Biloé <maxime.biloe@gmail.com>
2016-08-22 11:01:02 +0200

    // Version updated to v1.1.0

--- ps_legalcompliance.php
+++ ps_legalcompliance.php
@@ -63,7 +63,7 @@ class Ps_LegalCompliance extends Module
     {
         $this->name = 'ps_legalcompliance';
         $this->tab = 'administration';
-        $this->version = '1.0.1';
+        $this->version = '1.1.0';
         $this->author = 'PrestaShop';
         $this->need_instance = 0;
         $this->bootstrap = true;
