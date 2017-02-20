2592d37ec048f5c06008c4bf878ff39ab067e0be
Alex Sampaio <alex.sampaio1991@gmail.com>
2017-02-20 13:57:20 +0100

    // Version updated to v2.0.1

--- ps_legalcompliance.php
+++ ps_legalcompliance.php
@@ -64,7 +64,7 @@ class Ps_LegalCompliance extends Module
     {
         $this->name = 'ps_legalcompliance';
         $this->tab = 'administration';
-        $this->version = '2.0.0';
+        $this->version = '2.0.1';
         $this->author = 'PrestaShop';
         $this->need_instance = 0;
         $this->bootstrap = true;
