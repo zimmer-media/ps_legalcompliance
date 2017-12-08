44e88e2c0b339bbc9eae7929cc7cf9f266984582
Pablo Borowicz <eternoendless@gmail.com>
2017-12-08 11:30:34 +0100

    // Version updated to 3.0.0

--- ps_legalcompliance.php
+++ ps_legalcompliance.php
@@ -64,7 +64,7 @@ class Ps_LegalCompliance extends Module
     {
         $this->name = 'ps_legalcompliance';
         $this->tab = 'administration';
-        $this->version = '2.0.4';
+        $this->version = '3.0.0';
         $this->author = 'PrestaShop';
         $this->need_instance = 0;
         $this->bootstrap = true;
