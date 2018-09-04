17e3f11b5e294b4e2f08e1a7a675f55fef336a43
Thomas Nabord <thomas.nabord@prestashop.com>
2018-09-04 15:45:58 +0100

    Bump to v3.0.1

--- ps_legalcompliance.php
+++ ps_legalcompliance.php
@@ -64,7 +64,7 @@ class Ps_LegalCompliance extends Module
     {
         $this->name = 'ps_legalcompliance';
         $this->tab = 'administration';
-        $this->version = '3.0.0';
+        $this->version = '3.0.1';
         $this->author = 'PrestaShop';
         $this->need_instance = 0;
         $this->bootstrap = true;
